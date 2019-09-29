37350f619e827b30d162882f7600b37af60b8d32
j-buss <jeremy.f.buss@gmail.com>
2019-09-29 18:41:56 -0500

    updated randomQuizGenerator

--- chapter08/randomQuizGenerator.py
+++ chapter08/randomQuizGenerator.py
@@ -25,8 +25,8 @@ capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
 # Generate 35 quiz files
 for quizNum in range(35):
     # Create the quiz and answer key files.
-    quizFile = open('capitalsquiz%s.txt' % (quizNum + 1), 'w')
-    answerKeyFile = open('capitalsquiz_answers%s.txt' % (quizNum + 1), 'w')
+    quizFile = open('randomQuizGenerator/capitalsquiz%s.txt' % (quizNum + 1), 'w')
+    answerKeyFile = open('randomQuizGenerator/capitalsquiz_answers%s.txt' % (quizNum + 1), 'w')
 
     # Write out the header for the quiz.
     quizFile.write('Name:\n\nDate:\n\nPeriod:\n\n')
@@ -41,15 +41,22 @@ for quizNum in range(35):
     for questionNum in range(50):
 
         # Get right and wrong answers.
-        correctAnswers = capitals[states[questionNum]]
+        correctAnswer = capitals[states[questionNum]]
         wrongAnswers = list(capitals.values())
         del wrongAnswers[wrongAnswers.index(correctAnswer)]
         wrongAnswers = random.sample(wrongAnswers, 3)
         answerOptions = wrongAnswers + [correctAnswer]
         random.shuffle(answerOptions)
 
-        # TODO: Write the question and answer options to the quiz file.
+        # Write the question and answer options to the quiz file.
+        quizFile.write('%s. What is the capital of %s?\n' % (questionNum + 1, states[questionNum]))
+        for i in range(4):
+            quizFile.write(' %s. %s\n' % ('ABCD'[i], answerOptions[i]))
+        quizFile.write('\n')
 
-        # TODO: Write the answer key to a file.
+        # Write the answer key to a file.
+        answerKeyFile.write('%s. %s\n' % (questionNum + 1, 'ABCD'[answerOptions.index(correctAnswer)]))
+    quizFile.close()
+    answerKeyFile.close()
 
 
