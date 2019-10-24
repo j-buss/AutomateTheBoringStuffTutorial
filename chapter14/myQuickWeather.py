0f01a07b882ce3bfb1d48045aaac271c1b0f0651
j-buss <jeremy.f.buss@gmail.com>
2019-10-24 16:57:41 -0500

    Added my version of myQuickWeather.py

--- chapter14/myQuickWeather.py
+++ chapter14/myQuickWeather.py
@@ -6,23 +6,50 @@ import sys
 import requests
 import config_OWM
 import collections
-#from datetime import timedelta
 import datetime
 from dateutil.parser import parse
 
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
-#logging.disable(logging.CRITICAL)
+logging.disable(logging.CRITICAL)
+
+def convertKelvinToFahrenheit(inputKelvin):
+    celsius = inputKelvin - 273.15
+    logging.debug(celsius)
+    fahrenheit = (9/5 * celsius) + 32
+    logging.debug(fahrenheit)
+    return round(fahrenheit,2)
+
+def printCurrent(currentJSON):
+    print("\n----------------------")
+    print("Forecast for: " + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+    print("\t" + currentJSON["weather"][0]["description"])
+    kelvinTemp = convertKelvinToFahrenheit(currentJSON["main"]["temp"])
+    logging.debug("\tTemperature (K): " + str(currentJSON["main"]["temp"]))
+    print("\tTemperature (F): " + str(convertKelvinToFahrenheit(currentJSON["main"]["temp"])))
+    logging.debug("\n-----------------")
+    logging.debug(currentJSON)
+    return None
 
 def printForecast(forecastJSON):
-#    import datetime
-    t = datetime.time(hour=12,minute=0)
+    targetTime = datetime.time(hour=12,minute=0)
+    targetForecasts = []
     tomorrow = datetime.date.today() + datetime.timedelta(days=1)
-    targetDT = datetime.datetime.combine(tomorrow,t)
-    logging.debug("Target Date Time for tomorrow: " + str(targetDT))
+    tomorrow2 = tomorrow + datetime.timedelta(days=1)
+    targetDT = datetime.datetime.combine(tomorrow,targetTime)
+    targetDT2 = datetime.datetime.combine(tomorrow2,targetTime)
+    targetForecasts.append(targetDT)
+    targetForecasts.append(targetDT2)
+
     for forecast in forecastJSON["list"]:
-        logging.debug(forecast["dt_txt"])
-        if targetDT == parse(forecast["dt_txt"]):
-            logging.debug("Found Match")
+        if parse(forecast["dt_txt"]) in targetForecasts:
+            logging.debug("Found Match: " + str(targetDT))
+            logging.debug(forecast)
+            print("\n----------------------")
+            print("Forecast for: " + forecast["dt_txt"])
+            print("\t" + forecast["weather"][0]["description"])
+            kelvinTemp = forecast["main"]["temp"]
+            logging.debug("\tTemperature (K): " + str(kelvinTemp))
+            print("\tTemperature (F): " + str(convertKelvinToFahrenheit(kelvinTemp)))
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -43,8 +70,8 @@ if __name__ == "__main__":
     r = requests.get(URL, params=params)
     r.raise_for_status()
     
-    #logging.debug(r.url)
-    #logging.debug(r.text)
-
     weatherData = json.loads(r.text)
-    printForecast(weatherData)
+    if apiName == "forecast":
+        printForecast(weatherData)
+    else:
+        printCurrent(weatherData)
